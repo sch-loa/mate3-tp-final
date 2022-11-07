a0920eaca9f8a4483472535cacf79ebc9623aec8
sch-loa <laschleichgarcia@estudiantes.unsam.edu.ar>
2022-11-07 01:00:57 -0300

    ahora si me decidí

--- programa/main.py
+++ programa/main.py
@@ -4,42 +4,70 @@ Hecho por Loana Abril Schleich Garcia, entregado el dia xx/11/22.
 | DATOS PARA EL ANALISIS |
 |________________________|
     
-    Se seleccionó el archivo "BaseUnificadaEstaciones.csv",
-    de la fuente "https://data.buenosaires.gob.ar/dataset/subte-viajes-molinetes",
-    recurso encontrable en la sección "Recursos del dataset" como
-    "Cantidad de pasajeros por estación cada 15 minutos del año 2020".
+    Se seleccionó el archivo "measures.csv".
+ _____________________________
+| INTERPRETACIÓN DE LOS DATOS |
+|_____________________________|
 
-    De los datos proporcionados se contempla la cantidad de pasajeros de subte
-    por línea y por estación en una fecha, hora y rango horario determinado.
+    El archivo consta de 16 columnas; una actuando de índice y
+    15 columnas con datos de distintos sujetos relacionados
+    a su corporalidad, listados a continuación de forma ordenada:
     
-    Distinción entre hora y rango horario: 
-        Un rango horario comprende un conjunto de horas (véase columna "DESDE" y "HORA" respectivamente).
-        Ejemplo: Las horas 8:00, 8:15, 8:30 y 8:45 AM exclusivamente pertenecen al rango de las 8 AM.
+    densidad corporal, grasa corporal, edad, peso, estatura, cuello,
+    pecho, abdomen, cadera, muslo, rodilla, tobillo, bíceps,
+    antebrazo, muñeca.
+    
+    Las últimas 10 columnas anteriores corresponden a medidas corporales.
+    
+    Las unidades son ignorables para la realización de los cálculos, pues se asume que
+    se usan las mismas para cada columna.
+    
+ ________________________
+| OBJETIVOS Y ESTRATEGIA |
+|________________________|
 
-    El objetivo del análisis es predecir la cantidad de pasajeros en cada línea,
-    a continuación identificamos si existen datos no esenciales para la realización de los cálculos:
+    Mediante el procesamiento del conjunto de datos seleccionado,
+    se tiene como propósito producir predicciones en relación a la edad
+    de las personas, teniendo como base sus distintas características físicas.
     
+    En primera instancia, se deberá seleccionar cuidadosamente la información
+    relevante para el análisis: se toma la decisión de descartar la densidad
+    y la grasa corporal.
+    La primera hace referencia a la cantidad de masa
+    corporal libre de grasa en el cuerpo, mientras que la segunda es lo contrario.
     
-    
-   
+    La información que puede sacarse de esos datos es únicamente si el sujeto en cuestión
+    se encuentra en un estado físico óptimo en relación al peso. Dado que es una caractaristica
+    alterable, que puede oscilar en cortos periodos de tiempo debido a distintos factores
+    (ambientales, alimenticios, genéticos, etc) ambos son datos que realmente no aportan
+    información nueva ni relevante al objetivo.
 
+    Por otro lado, las medidas físicas sí resultan necesarias: Si bien varían de sujeto a sujeto
+    pudiendo haber tendencias producto de factores externos, son caracteristicas que se desarrollan
+    a medida que un ser vivo crece. Entonces es posible definir una tendencia dentro de un rango etario.
 """
 from clases import DataFrameManager
 from clases import DataCleaner
+from clases import TrainAndTest
 
 #dfFile = DataFrameManager("../datos/BaseUnificadaEstaciones.csv")
 dfFile = DataFrameManager("../datos/Datos.csv")
 cleaner = DataCleaner()
 
-dfFile.set_dependent_variables(["Compro"])
-dfFile.set_independent_variables(["Pais","Edad","Salario"])
-
-dfFile.set_data_frame(cleaner.fill_nan_values(dfFile.get_independent_variables(), ["Edad","Salario"]))
-dfFile.set_data_frame(cleaner.transform_categorical_data(dfFile.get_independent_variables(),["Pais"]))
-
-dfFile.set_data_frame(cleaner.transform_categorical_data(dfFile.get_dependent_variables(),["Compro"]))
 
 
+dfFile.set_data_frame(cleaner.fill_nan_values(dfFile.get_data_frame(), ["Edad","Salario"]))
+dfFile.set_data_frame(cleaner.transform_categorical_data(dfFile.get_data_frame(),["Pais"]))
 
+dfFile.set_data_frame(cleaner.transform_categorical_data(dfFile.get_data_frame(),["Compro"]))
+
+dfFile.set_dependent_variables(["Compro_1", "Compro_2"])
+dfFile.set_independent_variables(["Pais_1", "Pais_2", "Pais_3","Edad","Salario"])
+
+print(dfFile.get_data_frame())
 print(dfFile.get_independent_variables())
 print(dfFile.get_dependent_variables())
+
+tt = TrainAndTest(dfFile.get_independent_variables(),dfFile.get_dependent_variables())
+print("#####################################")
+print(tt.get_indepedent_train_values())
