c3de2570f202f95c2d6b9a564c6e26462214129a
sch-loa <laschleichgarcia@estudiantes.unsam.edu.ar>
2022-11-08 05:25:15 -0300

    hola jeje

--- programa/clases.py
+++ programa/clases.py
@@ -12,6 +12,7 @@ warnings.filterwarnings('ignore')
 ##########################################
 # Manejador del archivo de extensión csv #
 ##########################################
+
 class DataFrameManager:
     #Lee un archivo csv de ruta _path, separando las columnas con _delimitador.
     def __init__(self, _path, _delimiter):
@@ -29,21 +30,19 @@ class DataFrameManager:
 ########################################
 # Limpiador de los datos del DataFrame #
 ########################################
+
 class DataCleaner:
     #Retorna el DataFrame pasado como parámetro, modificando las filas
     #que tienen valores NaN con el método del valor medio.
     def fill_nan_values(self, data_frame):
-        #try:
         simple_imputer = SimpleImputer(missing_values = np.nan, strategy = "mean")
         data_frame = simple_imputer.fit_transform(data_frame)
-        #except:
-            #print("El conjunto posee columnas con datos no numéricos.")
-
         return data_frame
 
 #####################################################
 # Manejador de variables de entrenamiento y pruebas #
 #####################################################
+
 class TrainAndTest:
     #Particiona la matriz de variables independientes y el vector de variables
     #dependientes en sus correspondientes conjuntos de entrenamiento y prueba.
@@ -61,7 +60,7 @@ class TrainAndTest:
     #Retorna el conjunto de entrenamiento de las variables dependientes.
     def get_dependent_train_values(self):
         return self.__dep_train
-    
+
     #Retorna el conjunto de prueba de las variables dependientes.
     def get_dependent_test_values(self):
         return self.__dep_test
@@ -71,7 +70,7 @@ class TrainAndTest:
         self.__ind_test = StandardScaler().fit_transform(self.__ind_test)
         self.__dep_train = StandardScaler().fit_transform(self.__dep_train)
         self.__dep_test = StandardScaler().fit_transform(self.__dep_test)
-    
+   
     #Retorna la predicción obtenida a partir de los conjuntos de prueba.
     def get_predict(self):
         regression = LinearRegression().fit(self.__ind_train, self.__dep_train)
--- programa/main.py
+++ programa/main.py
@@ -9,25 +9,27 @@ Hecho por Loana Abril Schleich Garcia, entregado el dia xx/11/22.
 | INTERPRETACIÓN DE LOS DATOS |
 |_____________________________|
 
-    El archivo consta de 16 columnas; una actuando de índice y
-    15 columnas con datos de distintos sujetos relacionados
-    a su corporalidad, listados a continuación de forma ordenada:
+    El archivo consta de 15 columnas (desestimando la primera que actúa de índice),
+    listadas a continuación de forma ordenadacon una nreve descripción:
     
-    density: densidad corporal, relación entre la masa de un cuerpo y el volumen que ocupa.
-    fat: grasa corporal
-    age: edad
-    weight: peso del sujeto
-    height: altura del sujeto
-    neck: medida de la circunferencia del cuello
-    chest: ídem anterior, del pecho
-    abdomen: ídem anterior, del abdomen
-    hip: ídem anterior, pero de la cadera
-    thigh: ídem anterior, pero del muslo
-    knee: ídem anterior, pero de la rodilla
-    ankle: ídem anterior, del tobillo.
-    bicep: ídem, bícep
-    forearm: ídem, antebrazo
-    wrist: ídem, muñeca
+    1. density: Densidad corporal, relación entre la masa de un cuerpo y el volumen que ocupa.
+    2. fat: Grasa corporal en la totalidad de la masa.
+    3. age: Edad
+    4. weight: Peso
+    5. height: Estatura
+    
+    Columnas con las medidas de la circunferencia de:
+    
+    6. neck: Cuello
+    7. chest: Pecho
+    8. abdomen: Abdomen
+    9. hip: Cadera
+    10. thigh: Muslo
+    11. knee: Rodilla
+    12. ankle: Tobillo
+    13. bicep: Bícep
+    14. forearm: Antebrazo
+    15. wrist: Muñeca
     
     Las unidades son ignorables para la realización de los cálculos, pues se asume que
     se usan las mismas para cada columna.
@@ -41,9 +43,35 @@ Hecho por Loana Abril Schleich Garcia, entregado el dia xx/11/22.
     como base sus distintas características físicas.
     
     En primera instancia, se deberá seleccionar cuidadosamente la información
-    relevante para el análisis: se toma la decisión .
+    relevante para el análisis: a continuaciónse listan las columnas
+    seleccionadas y su fundamentación:
+    
+    VARIABLES DEPENDIENTES:
+        Se elige la columna 4.
+        El peso es el dato que se desea predecir y cuyo valor suponemos varía en relación al resto.
 
+    VARIABLES INDEPENDIENTES:
+        Se eligen las columnas 2, 3, y de 5 a 15.
+   
+        El peso está fuertemente ligado a la cantidad de masa de la que se compone el cuerpo,
+        ya sea grasa, músculo o hueso. El porcentaje de grasa en el cuerpo es esencial para realizar
+        los cálculos, un sujeto con mayor o menor porcentaje de grasa en su cuerpo va a tender a un
+        mayor o menor peso. Un mayor o menor peso a su vez significa un aumento o decremento en las medidas
+        físicas debido a los cambios en el volumen corporal.
+        
+        La edad es un factor importante, el envejecimiento y los cambios hormonales y metabólicos
+        que supone favorecen el aumento de peso.
+        
+        La estatura también es un dato a tener en cuenta, un objeto más alto tiende a ser más pesado debido a su volumen.
+        La masa ósea y muscular por sí misma es mayor y aumenta considerablemente su peso, teniendo o no en cuenta
+        la cantidad de grasa corporal que pueda poseer.
+        
+        La densidad, por otro lado, es el cociente entre la masa y el volumen que ocupa, que un cuerpo sea
+        muy denso sólo significa que es más compacto que otro con exactamente el mismo peso con menor densidad.
+
+    Dado que se tienen múltiples variables independientes para el análisis, se utilizará un modelo de Regresión Lineal Múltiple.
 """
+
 from matplotlib import pyplot as plt
 from sklearn import metrics
 import numpy as np
@@ -54,8 +82,6 @@ from clases import TrainAndTest
 
 import seaborn
 
-from sklearn.metrics import r2_score
-
 #################################
 # PREPROCESAMIENTO DE LOS DATOS #
 #################################
@@ -64,7 +90,7 @@ from sklearn.metrics import r2_score
 data_frame = DataFrameManager("../datos/measures.csv", ";")
 
 #Creo subconjuntos de variables independientes y dependientes.
-independent_vars = data_frame.get_sub_data_frame([1,2,3,5,6,7,8,9,10,11,12,13,14,15])
+independent_vars = data_frame.get_sub_data_frame([2,3,5,6,7,8,9,10,11,12,13,14,15])
 dependent_vars = data_frame.get_sub_data_frame([4])
 
 #Limpio datos numéricos en caso de que hayan valores nulos
@@ -72,27 +98,29 @@ independent_vars = DataCleaner().fill_nan_values(independent_vars)
 dependent_vars = DataCleaner().fill_nan_values(dependent_vars)
 
 tt = TrainAndTest(independent_vars, dependent_vars)
+"""
 #tt.scale_all_data()
+"""
 pred_vals = tt.get_predict()
 test_vals = tt.get_dependent_test_values()
 
-print(f"{tt.compare_test_and_predict()} \n")
+#print(f"{tt.compare_test_and_predict()} \n")
 
 ######################
 # GRAFICO RESULTADOS #
 ######################
 
-#Creo gráfico de barras para comparar las variables de prueba y las predicciones para visualizar la precisión.
-tt.compare_test_and_predict().plot(kind = "bar", figsize = (10,5), color = ["blue","mediumpurple"])
-
-plt.title("Diferencia entre los valores reales y los de la predicción")
+#Creo gráfico de barras para comparar las variables de prueba y
+#las predicciones para visualizar la precisión.
+tt.compare_test_and_predict().plot(kind = "bar", figsize = (10,5), color = ["skyblue","plum"], width = 0.8)
+plt.title("Valores reales vs. Predicciones")
 plt.show()
 
 #Creo gráfico de regresión lineal
-seaborn.regplot(x = test_vals, y = pred_vals, scatter_kws = {"color": "mediumpurple"}, line_kws = {"color": "black"})
+seaborn.regplot(x = test_vals, y = pred_vals, scatter_kws = {"color": "plum"}, line_kws = {"color": "black"})
 plt.xlabel("Actual")
 plt.ylabel("Predicción")
-plt.title("Predicciones en un modelo de RLM")
+plt.title("Predicciones en un modelo RLM")
 plt.show()
 
 print("Media del peso de los sujetos:", np.round_(np.mean(dependent_vars), decimals = 2), "\n")
