827bc1b00ccd68fa115b9d9203a8f826ca9ee74d
sch-loa <laschleichgarcia@estudiantes.unsam.edu.ar>
2022-11-08 00:41:57 -0300

    hols jiji

--- programa/clases.py
+++ programa/clases.py
@@ -9,20 +9,29 @@ from sklearn.linear_model import LinearRegression
 import warnings
 warnings.filterwarnings('ignore')
 
-"""
-Clase para manejar el archivo csv.
-"""
+##########################################
+# Manejador del archivo de extensión csv #
+##########################################
 class DataFrameManager:
+    #Lee un archivo csv de ruta _path, separando las columnas con _delimitador.
     def __init__(self, _path, _delimiter):
         self.__data_frame = pd.read_csv(_path, sep = _delimiter)
     
+    #Retorna el DataFrame
     def get_data_frame(self):
         return self.__data_frame
     
+    #Retorna todas las filas de una partición del DataFrame,
+    #pasando como parámetro una lista con los índices de las columnas.
     def get_sub_data_frame(self, list_of_colums):
         return self.__data_frame.iloc[:,list_of_colums]
 
+########################################
+# Limpiador de los datos del DataFrame #
+########################################
 class DataCleaner:
+    #Retorna el DataFrame pasado como parámetro, modificando las filas
+    #que tienen valores NaN con el método del valor medio.
     def fill_nan_values(self, data_frame):
         #try:
         simple_imputer = SimpleImputer(missing_values = np.nan, strategy = "mean")
@@ -32,22 +41,28 @@ class DataCleaner:
 
         return data_frame
 
-    def scale_data(self, array_vars):
-        return StandardScaler().fit_transform(array_vars)
-
+#####################################################
+# Manejador de variables de entrenamiento y pruebas #
+#####################################################
 class TrainAndTest:
+    #Particiona la matriz de variables independientes y el vector de variables
+    #dependientes en sus correspondientes conjuntos de entrenamiento y prueba.
     def __init__(self, independent_vars, dependent_vars):       
-        self.__ind_train, self.__ind_test, self.__dep_train, self.__dep_test = train_test_split(independent_vars, dependent_vars, test_size = 0.2, random_state = 10)
-
+        self.__ind_train, self.__ind_test, self.__dep_train, self.__dep_test = train_test_split(independent_vars, dependent_vars, test_size = 0.2, random_state = 0)
+    
+    #Retorna el conjunto de entrenamiento de las variables independientes.
     def get_independent_train_values(self):
         return self.__ind_train
-
+    
+    #Retorna el conjunto de prueba de las variables independientes.
     def get_independent_test_values(self):
         return self.__ind_test
     
+    #Retorna el conjunto de entrenamiento de las variables dependientes.
     def get_dependent_train_values(self):
         return self.__dep_train
-
+    
+    #Retorna el conjunto de prueba de las variables dependientes.
     def get_dependent_test_values(self):
         return self.__dep_test
 
@@ -56,11 +71,14 @@ class TrainAndTest:
         self.__ind_test = StandardScaler().fit_transform(self.__ind_test)
         self.__dep_train = StandardScaler().fit_transform(self.__dep_train)
         self.__dep_test = StandardScaler().fit_transform(self.__dep_test)
-        
+    
+    #Retorna la predicción obtenida a partir de los conjuntos de prueba.
     def get_predict(self):
         regression = LinearRegression().fit(self.__ind_train, self.__dep_train)
         return regression.predict(self.__ind_test)
     
-    def compare_train_and_test(self):
-        return pd.DataFrame({"Actual": np.reshape(self.__dep_test.flatten(), -1), "Prediccion": np.reshape(self.get_predict(), -1)})
+    #Retorna un DataFrame con los datos de prueba y los datos obtenidos
+    #a partir de las predicciones como columnas.
+    def compare_test_and_predict(self):
+        return pd.DataFrame({"Actual": np.reshape(self.__dep_test.flatten(), -1), "Prediccion": np.reshape(np.round_(self.get_predict(), decimals = 2), -1)})
     
--- programa/main.py
+++ programa/main.py
@@ -37,43 +37,35 @@ Hecho por Loana Abril Schleich Garcia, entregado el dia xx/11/22.
 |________________________|
 
     Mediante el procesamiento del conjunto de datos seleccionado,
-    se tiene como propósito producir predicciones en relación a la edad
-    de las personas, teniendo como base sus distintas características físicas.
+    se tiene como propósito predecir el peso de las personas, teniendo
+    como base sus distintas características físicas.
     
     En primera instancia, se deberá seleccionar cuidadosamente la información
-    relevante para el análisis: se toma la decisión de descartar la densidad
-    y la grasa corporal.
-    La primera hace referencia a la cantidad de masa
-    corporal libre de grasa en el cuerpo, mientras que la segunda es lo contrario.
-    
-    La información que puede sacarse de esos datos es únicamente si el sujeto en cuestión
-    se encuentra en un estado físico óptimo en relación al peso. Dado que es una caractaristica
-    alterable, que puede oscilar en cortos periodos de tiempo debido a distintos factores
-    (ambientales, alimenticios, genéticos, etc) ambos son datos que realmente no aportan
-    información nueva ni relevante al objetivo.
+    relevante para el análisis: se toma la decisión .
 
-    Por otro lado, las medidas físicas sí resultan necesarias: Si bien varían de sujeto a sujeto
-    pudiendo haber tendencias producto de factores externos, son caracteristicas que se desarrollan
-    a medida que un ser vivo crece. Entonces es posible definir una tendencia dentro de un rango etario.
 """
 from matplotlib import pyplot as plt
+from sklearn import metrics
+import numpy as np
 
 from clases import DataFrameManager
 from clases import DataCleaner
 from clases import TrainAndTest
 
+import seaborn
+
 from sklearn.metrics import r2_score
 
-###############################
-#PREPROCESAMIENTO DE LOS DATOS#
-###############################
+#################################
+# PREPROCESAMIENTO DE LOS DATOS #
+#################################
 
 #Creo objeto para manejar el conjunto de datos
 data_frame = DataFrameManager("../datos/measures.csv", ";")
 
 #Creo subconjuntos de variables independientes y dependientes.
-independent_vars = data_frame.get_sub_data_frame(list(range(4,16)))
-dependent_vars = data_frame.get_sub_data_frame([3])
+independent_vars = data_frame.get_sub_data_frame([1,2,3,5,6,7,8,9,10,11,12,13,14,15])
+dependent_vars = data_frame.get_sub_data_frame([4])
 
 #Limpio datos numéricos en caso de que hayan valores nulos
 independent_vars = DataCleaner().fill_nan_values(independent_vars)
@@ -81,20 +73,46 @@ dependent_vars = DataCleaner().fill_nan_values(dependent_vars)
 
 tt = TrainAndTest(independent_vars, dependent_vars)
 #tt.scale_all_data()
+pred_vals = tt.get_predict()
+test_vals = tt.get_dependent_test_values()
 
-####################
-#GRAFICO RESULTADOS#
-####################
+print(f"{tt.compare_test_and_predict()} \n")
 
-tt.compare_train_and_test().plot(kind = "bar", figsize = (10,5))
+######################
+# GRAFICO RESULTADOS #
+######################
+
+#Creo gráfico de barras para comparar las variables de prueba y las predicciones para visualizar la precisión.
+tt.compare_test_and_predict().plot(kind = "bar", figsize = (10,5), color = ["blue","mediumpurple"])
+
 plt.title("Diferencia entre los valores reales y los de la predicción")
 plt.show()
 
-plt.scatter(tt.get_dependent_test_values(), tt.get_predict())
-plt.plot(tt.get_dependent_test_values(),tt.get_predict())
+#Creo gráfico de regresión lineal
+seaborn.regplot(x = test_vals, y = pred_vals, scatter_kws = {"color": "mediumpurple"}, line_kws = {"color": "black"})
 plt.xlabel("Actual")
 plt.ylabel("Predicción")
-plt.title("Predicción usando Regresión Lineal Múltiple")
+plt.title("Predicciones en un modelo de RLM")
 plt.show()
 
-print(r2_score(tt.get_dependent_test_values(), tt.get_predict())*100)
+print("Media del peso de los sujetos:", np.round_(np.mean(dependent_vars), decimals = 2), "\n")
+print("Error Absoluto Medio:", round(metrics.mean_absolute_error(test_vals, pred_vals), 2))
+print("Error Cuadratico Medio:", round(metrics.mean_squared_error(test_vals, pred_vals), 2))
+print("Raíz del error cuadrático medio:", np.round_(np.sqrt(metrics.mean_squared_error(test_vals, pred_vals)), decimals = 2))
+
+"""
+ ______________
+| CONCLUSIONES |
+|______________|
+
+El gráfico de barras a simple vista mostró una gran cercanía entre
+los valores reales y las predicciones. No fue distinto con el gráfico de regresión lineal,
+donde las distintas relaciones entre las predicciones y los valores reales tienen una
+gran cercanía con la recta trazada.
+
+La raíz del error cuadrático medio nos dió un valor de 4.92, que equivale
+a sólo el 2.92% de la media del peso de los sujetos, un valor muy bajo. Esto equivale
+a una presición considerablemente alta.
+
+Se concluye que hubo una buena correlación entre los datos utilizados para la predicción  y el valor a predecir.
+"""
